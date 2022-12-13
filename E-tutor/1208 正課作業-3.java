91b02a8641ea8ffa11ed8ff350c0001827426305
kevin083177 <kevin083177@gmail.com>
2022-12-14 01:03:35 +0800

    　　

--- E-tutor/1208 正課作業-3.java
+++ E-tutor/1208 正課作業-3.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 public class Main{      
     public static void main(String[] args){      
         Scanner input  = new Scanner(System.in);      
-        int []array = new int[6];  
-        int temp;  
-        for(int i=0; i<6; i++){  
-            array[i] = input.nextInt();  
-        }  
-        for(int i=0; i<5; i++){  
-            for(int j=0; j<5; j++){  
-                if(array[j]<array[j+1]){  
-                    temp = array[j];  
-                    array[j] = array[j+1];  
-                    array[j+1] = temp;  
-                }  
-            }  
-        }  
-        System.out.printf("%d %d\n", array[0], array[1]);  
+        int number = input.nextInt();
+        int max = 0;
+        int sec_max = 0;
+        while(number!=-1){
+            number = input.nextInt();
+            if(number > max){
+                max = number;
+            }
+            if(number < max && number > sec_max){
+                sec_max = number;
+            }
+        }
+        System.out.printf("%d %d\n", max, sec_max);
     }      
-} 
+}
