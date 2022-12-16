090f7a47b12962c64d657ec5af98c1b16e75517b
kevin083177 <kevin083177@gmail.com>
2022-12-17 01:09:55 +0800

    　　

--- E-tutor/Main.java
+++ E-tutor/Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Main{  //Test Data Error
+public class Main{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         double x1 = input.nextDouble();
         double y1 = input.nextDouble();
         double x2 = input.nextDouble();
         double y2 = input.nextDouble();
-        double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-        System.out.printf("The distance between the two points is %.15f\n", distance);
+        double distance = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+        System.out.println("The distance between the two points is " + distance);
      }
 }
