4bfdcad5ed1fec0929f7cf102107f179dca818be
kevin083177 <kevin083177@gmail.com>
2022-12-17 00:09:46 +0800

    　　

--- E-tutor/20221220作業_靠右直角三角形（輸入版）.java
+++ E-tutor/20221220作業_靠右直角三角形（輸入版）.java
@@ -3,15 +3,14 @@ public class Main{
     public static void main(String[] args){    
         Scanner input  = new Scanner(System.in);  
         int number = input.nextInt();  
-        char star = '*';  
         for(int i=0; i<number; i++){  
             for(int k=0; k<number-i-1; k++){  
-                System.out.printf(" ");  
+                System.out.print(" ");  
             }  
             for(int j=0; j<=i; j++){  
-                System.out.printf("%c", star);  
+                System.out.print("*");  
             }  
-            System.out.printf("\n");  
+            System.out.print("\n");  
         }  
     }  
 } 
