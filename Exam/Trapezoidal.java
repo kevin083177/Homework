95c2e9e126cbe0bb8e78c53963fe8c680402fbf2
kevin083177 <kevin083177@gmail.com>
2022-12-28 00:49:35 +0800

    　　

--- Exam/Trapezoidal.java
+++ Exam/Trapezoidal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Trapezoidal{    
+public class trapezoidal{    
     public static void main(String[] args){    
         Scanner input  = new Scanner(System.in);  
         int upper = input.nextInt();
