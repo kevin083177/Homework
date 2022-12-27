71dd3e74f217fa72ecb0a94cc129b8ea68bbd0a9
kevin083177 <kevin083177@gmail.com>
2022-12-28 00:50:02 +0800

    　　

--- Exam/align.java
+++ Exam/align.java
@@ -3,7 +3,7 @@ public class align{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
-        for(int i=0; i<number; i++){
+        for(int i=0; i<=number; i++){
             System.out.printf("%-5d%-5d%d\n", i, i*i, i*i*i);
         }
     }
