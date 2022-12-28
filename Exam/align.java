965082a28bafb4b53a68fe3cf290c2348b0acfea
kevin083177 <kevin083177@gmail.com>
2022-12-28 22:57:18 +0800

    　　

--- Exam/align.java
+++ Exam/align.java
@@ -4,7 +4,7 @@ public class align{
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
         for(int i=0; i<=number; i++){
-            System.out.printf("%-5d%-5d%d\n", i, i*i, i*i*i);
+            System.out.printf("%-5d%-5d%d\n", i, i*i, i*i*i); //% -靠左, 5給變數多少個位置, d整數
         }
     }
 }
--- Exam/max_time.java
+++ Exam/max_time.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;      
 public class max_time{      
+    //modified: 1208 正課作業-3
+    //output: max value and max appear time
     public static void main(String[] args){      
         Scanner input  = new Scanner(System.in);      
         int number = input.nextInt();
         int max = number;
         //int sec_max = number;
         int count = 0;
-        while(number!=-1){
+        while(number!=-1){ //輸入-1停止
             number = input.nextInt();
             if(number > max){
                 //sec_max = max;
                 max = number;
-                count = 0;
+                count = 0; //最大值改變 計數重設
             }
-            if(number == max){
+            if(number == max){  //在第一次的時候因為number==max所以count=1
                 count++;
             }
             /*
@@ -23,6 +25,5 @@ public class max_time{
             */
         }
         System.out.printf("max = %d, count = %d\n", max, count);
-        input.close();
     }      
 }
