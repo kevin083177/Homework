2a01b9025fc98e395e41c98d40cd2da7fc56b6e4
kevin083177 <kevin083177@gmail.com>
2022-12-28 21:59:44 +0800

    　　

--- Exam/trapezoidal_string.java
+++ Exam/trapezoidal_string.java
@@ -6,19 +6,19 @@ public class trapezoidal_string {
         int c = input.nextInt(); //上底
         int n = input.nextInt(); //下底
         if(c<n){ //上底比較短
-            for(int i=c; i<=n; i++){ //靠右正梯形
+            for(int i=c; i<=n; i++){ //靠右正梯形 i:控制星星
                 System.out.println(" ".repeat(n-i) + "*".repeat(i));
             }
-            System.out.println("----------");  //間隔
+            System.out.println("");
             for(int i=c; i<=n; i++){ //靠左正梯形
                 System.out.println("*".repeat(i));
             }
         }
         else{ //上底比較長
-            for(int i=0; i<n; i++){ //靠右倒梯形
+            for(int i=0; i<n; i++){ //靠右倒梯形 i:控制空格
                 System.out.println(" ".repeat(i) + "*".repeat(c-i));
             }
-            System.out.println("----------"); //間隔
+            System.out.println("");
             for(int i=0; i<n; i++){ //靠左倒梯形
                 System.out.println("*".repeat(c-i));
             }
